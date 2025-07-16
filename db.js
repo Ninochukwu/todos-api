9a8ce93dde66f7b70b59618f1050b3a9368b8e18
Chinecherem Ugbelu <chinecheremugbelu@gmail.com>
2025-07-16 15:16:15 +0100

    .

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todos-api',
+  {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
